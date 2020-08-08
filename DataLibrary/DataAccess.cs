9c4db06ee181d00e78a7d337361ef3684d703b3e
jason-parson <jpsells4u@gmail.com>
2020-08-08 12:34:21 -0500

    Add project files.

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace DataLibrary
+{
+    public class DataAccess
+    {
+        public static List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString)
+            { 
+              
+            }
+
+        }
+    }
+}
